3ce6f21ec0a655b9583248c3e66e4f5dc0c2bde7
joko751214 <joko751214@gmail.com>
2022-07-03 12:37:59 +0800

    修改 api 路徑

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:stark920/TeamWall.git main:gh-pages
+git push -f https://github.com/joko751214/TeamWall.git main:gh-pages
 
 cd -
